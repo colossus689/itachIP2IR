8e8fed56c95b9888dfb5c67539c654e4a8e69843
colossus689 <colossus@whiteshark.local>
2014-04-19 17:13:44 -0400

    added files, ignore and README

---
+++ itachIP2IR/itachIP2IR.h
@@ -0,0 +1,13 @@
+//
+//  itachIP2IR.h
+//  itachIP2IR
+//
+//  Created by colossus on 4/19/14.
+//  Copyright (c) 2014 colossus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface itachIP2IR : NSObject
+
+@end
---
+++ itachIP2IR/itachIP2IR.m
@@ -0,0 +1,13 @@
+//
+//  itachIP2IR.m
+//  itachIP2IR
+//
+//  Created by colossus on 4/19/14.
+//  Copyright (c) 2014 colossus. All rights reserved.
+//
+
+#import "itachIP2IR.h"
+
+@implementation itachIP2IR
+
+@end
